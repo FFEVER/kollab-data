4486f1dcf22f72f305f81210b4550321d904121e
Nattaphol <rootncdev2015@gmail.com>
2020-04-22 03:03:52 +0700

    [+] Add project test has many expertises

--- kollabStorage/tests.py
+++ kollabStorage/tests.py
@@ -20,3 +20,22 @@ class UserTestCase(TestCase):
         e2 = Expertise.objects.get(id=1)
         self.assertIn(e1, u1.expertises.all())
         self.assertIn(e2, u1.expertises.all())
+
+
+class ProjectTestCase(TestCase):
+    def setUp(self):
+        t = timezone.now().isoformat()
+        p = Project.objects.create(
+            id=1, title="Title 1", created_at=t, updated_at=t)
+        e1 = Expertise.objects.create(id=1, name="Mathematics")
+        e2 = Expertise.objects.create(id=2, name="Sciences")
+        p.expertises.add(e1)
+        p.expertises.add(e2)
+
+    def test_project_has_many_expertises(self):
+        """Projects should has many Expertise"""
+        p1 = Project.objects.get(id=1)
+        e1 = Expertise.objects.get(id=1)
+        e2 = Expertise.objects.get(id=1)
+        self.assertIn(e1, p1.expertises.all())
+        self.assertIn(e2, p1.expertises.all())
