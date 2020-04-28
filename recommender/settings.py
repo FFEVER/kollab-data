d79ff10b1cb29388d16233602d1610ff5bca738f
Nattaphol <rootncdev2015@gmail.com>
2020-04-29 01:25:03 +0700

    [+] Add database to travis

--- recommender/settings.py
+++ recommender/settings.py
@@ -75,17 +75,28 @@ WSGI_APPLICATION = 'recommender.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('KOLLAB_DATA_DATABASE_NAME'),
-        'USER': os.getenv('KOLLAB_DATA_DATABASE_USERNAME'),
-        'PASSWORD': os.getenv('KOLLAB_DATA_DATABASE_PASSWORD'),
-        'HOST': os.getenv('KOLLAB_DATA_DATABASE_HOST'),
-        'PORT': '5432',
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'travisci',
+            'USER': 'postgres',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.getenv('KOLLAB_DATA_DATABASE_NAME'),
+            'USER': os.getenv('KOLLAB_DATA_DATABASE_USERNAME'),
+            'PASSWORD': os.getenv('KOLLAB_DATA_DATABASE_PASSWORD'),
+            'HOST': os.getenv('KOLLAB_DATA_DATABASE_HOST'),
+            'PORT': '5432',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
