3cdb51ad70e3e6def80c13acf42207f5cffe051f
Nattaphol <rootncdev2015@gmail.com>
2020-05-19 01:22:38 +0700

    [~] Modify models

--- apps/kstorage/models.py
+++ apps/kstorage/models.py
@@ -7,8 +7,8 @@ class User(models.Model):
     email = models.EmailField(default="default@kollab.default")
     role = models.CharField(max_length=50, null=True)
     faculty = models.IntegerField(null=True)
-    expertises = ArrayField(ArrayField(models.IntegerField(blank=True, null=True)))
-    skills = ArrayField(models.CharField(max_length=50))
+    expertises = ArrayField(ArrayField(models.IntegerField(blank=True, null=True)), default=list)
+    skills = ArrayField(models.CharField(max_length=50, blank=True, null=True), default=list)
     year = models.CharField(max_length=20, null=True)
 
     def __str__(self):
@@ -19,7 +19,8 @@ class Project(models.Model):
     id = models.PositiveIntegerField(primary_key=True)
     title = models.CharField(max_length=300)
     project_status = models.SmallIntegerField(default=1, null=True)
-    expertises = ArrayField(ArrayField(models.IntegerField(blank=True, null=True)))
+    categories = ArrayField(ArrayField(models.IntegerField(blank=True, null=True)), default=list)
+    tags = ArrayField(models.CharField(max_length=50, blank=True, null=True), default=list)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
 
--- apps/kstorage/tests.py
+++ apps/kstorage/tests.py
@@ -7,7 +7,7 @@ class UserTestCase(TestCase):
     def setUp(self):
         t = timezone.now().isoformat()
         u = User.objects.create(id=1, email="eit@gmail.com", role="student", faculty=1, year="1",
-                                expertises=[[1, 2, 3], [5, 6]])
+                                expertises=[[1, 2, 3], [5, 6, None]], skills=["a", "b", "c"])
 
     def test_user_has_correct_attributes(self):
         """Users should have correct attributes"""
@@ -15,18 +15,20 @@ class UserTestCase(TestCase):
         self.assertEqual("student", u1.role)
         self.assertEqual(1, u1.faculty)
         self.assertEqual("1", u1.year)
-        self.assertEqual([[1, 2, 3], [5, 6]], u1.expertises)
+        self.assertEqual([[1, 2, 3], [5, 6, None]], u1.expertises)
+        self.assertEqual(["a", "b", "c"], u1.skills)
 
 
 class ProjectTestCase(TestCase):
     def setUp(self):
         t = timezone.now().isoformat()
         p = Project.objects.create(id=1, title="Title 1", project_status=1, created_at=t, updated_at=t,
-                                   categories=[[1, 2, 3], [5, 6]])
+                                   categories=[[1, 2, 3], [5, 6, None]], tags=["a", "b", "c"])
 
     def test_project_has_many_expertises(self):
         """Projects should have correct attributes"""
         p1 = Project.objects.get(id=1)
         self.assertEqual("Title 1", p1.title)
         self.assertEqual(1, p1.project_status)
-        self.assertEqual([[1, 2, 3], [5, 6]], p1.expertises)
+        self.assertEqual([[1, 2, 3], [5, 6, None]], p1.categories)
+        self.assertEqual(["a", "b", "c"], p1.tags)
