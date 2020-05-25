911272d7f167f3c2c4e18bfdd3faa0b078e20e99
Nattaphol <rootncdev2015@gmail.com>
2020-05-25 22:56:54 +0700

    [~] Add project interactions to user model

--- apps/kstorage/models.py
+++ apps/kstorage/models.py
@@ -9,6 +9,10 @@ class User(models.Model):
     faculty_id = models.IntegerField(null=True)
     expertises = ArrayField(ArrayField(models.IntegerField(blank=True, null=True)), default=list)
     skills = ArrayField(models.CharField(max_length=50, blank=True, null=True), default=list)
+    joined_projects = ArrayField(models.IntegerField(blank=True, null=True), default=list)
+    starred_projects = ArrayField(models.IntegerField(blank=True, null=True), default=list)
+    viewed_projects = ArrayField(models.IntegerField(blank=True, null=True), default=list)
+    followed_projects = ArrayField(models.IntegerField(blank=True, null=True), default=list)
     year = models.CharField(max_length=20, null=True)
 
     def __str__(self):
