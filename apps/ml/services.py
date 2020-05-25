a896f4636aa578fcfc3766cfd55f70cdde3c20f0
Nattaphol <rootncdev2015@gmail.com>
2020-05-25 22:58:11 +0700

    [~] Refactor FieldSim and change name of UserProjectSimilarity

--- apps/ml/services.py
+++ apps/ml/services.py
@@ -1,4 +1,4 @@
-from research.project_recommender.project_recommender import UserProjectSimilarity
+from research.project_recommender.project_recommender import UserProjectRelationManager
 from apps.ml.models import UserProjectRelation
 
 
@@ -6,12 +6,13 @@ class ProjectRecommenderService:
 
     @staticmethod
     def create_sim_tables():
-        user_project_sim = UserProjectSimilarity()
+        user_project_sim = UserProjectRelationManager()
         user_project_sim.create_sim_table()
         user_project_sim.fill_sim_table()
         UserProjectRelation.objects.create(users_count=user_project_sim.get_users_count(),
                                            projects_count=user_project_sim.get_projects_count(),
-                                           data_frame=user_project_sim.get_picked_sim_table())
+                                           data_frame=user_project_sim.get_picked_sim_table(),
+                                           alg_type=)
 
     @classmethod
     def perform_precalculations(cls):
