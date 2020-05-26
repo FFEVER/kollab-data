26bc630d4aae41158704cfb448708a7c1de2c0c3
Nattaphol <rootncdev2015@gmail.com>
2020-05-26 22:43:08 +0700

    [+] Add pre-calculation for interactions based

--- apps/ml/project_recommender/interacted_projects_based.py
+++ apps/ml/project_recommender/interacted_projects_based.py
@@ -1,6 +1,9 @@
-from apps.kstorage.models import User
+from apps.kstorage.models import User, Project
 from apps.ml.models import Relation
 import pickle
+
+from research.project_recommender.relation_calculator import RelationCalcByInteractions
+from research.project_recommender.relationship import UserProjectRelationship, ProjectRelationship
 
 
 class InteractedProjectsBased:
@@ -11,6 +14,35 @@ class InteractedProjectsBased:
     version = "0.0.1"
     status = "development"
 
+    @classmethod
+    def pre_calculate(cls):
+        cls.create_user_project_relation_by_interactions()
+        cls.create_project_relation_by_fields()
+
+    @staticmethod
+    def create_project_relation_by_fields():
+        # Relation table for project/project
+        project_by_fields = ProjectRelationship()
+        project_by_fields.fill_relations()
+        Relation.objects.create(row_count=project_by_fields.row_count(),
+                                col_count=project_by_fields.col_count(),
+                                row_type=project_by_fields.row_type(),
+                                col_type=project_by_fields.col_type(),
+                                data_frame=project_by_fields.get_picked_relations(),
+                                alg_type=project_by_fields.alg_type())
+
+    @staticmethod
+    def create_user_project_relation_by_interactions():
+        # Relation table for past interactions based
+        user_project_interactions = UserProjectRelationship(calculator_class=RelationCalcByInteractions)
+        user_project_interactions.fill_relations()
+        Relation.objects.create(row_count=user_project_interactions.row_count(),
+                                col_count=user_project_interactions.col_count(),
+                                row_type=user_project_interactions.row_type(),
+                                col_type=user_project_interactions.col_type(),
+                                data_frame=user_project_interactions.get_picked_relations(),
+                                alg_type=user_project_interactions.alg_type())
+
     def preprocessing(self, input_data):
         user_id = input_data['user_id']
         if User.objects.filter(id=user_id).exists():
@@ -18,7 +50,8 @@ class InteractedProjectsBased:
         return -1
 
     def predict(self, input_data):
-        latest_relation = Relation.objects.last()
+        latest_relation = Relation.objects.filter(row_type=User.__name__, col_type=Project.__name__,
+                                                  alg_type=RelationCalcByInteractions.__name__).last()
         relation_df = pickle.loads(latest_relation.data_frame)
 
         if input_data == -1:
@@ -29,7 +62,8 @@ class InteractedProjectsBased:
     def postprocessing(self, prediction):
         prediction = prediction.melt().sort_values('value', ascending=False)
         top_100_projects = prediction.head(100)['variable'].to_list()
-        return {"projects": top_100_projects, "label": "top_related_projects", "status": "OK"}
+        return {"projects": top_100_projects, "label": "top_related_projects", "status": "OK",
+                "alg_name": InteractedProjectsBased.algorithm_name}
 
     def compute_prediction(self, input_data):
         try:
