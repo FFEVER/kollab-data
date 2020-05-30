deff49fc01ce8cd27e97c55581430736120e6865
Nattaphol <rootncdev2015@gmail.com>
2020-05-31 01:28:15 +0700

    [~] Fix root url not link to admin

--- recommender/urls.py
+++ recommender/urls.py
@@ -20,7 +20,7 @@ from apps.api.urls import urlpatterns as endpoints_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', admin.site.urls),
+    path('', admin.site.urls),
 ]
 
 urlpatterns += endpoints_urlpatterns
